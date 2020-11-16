4ab6fee8ef5aae194ff1e9d546851d955df047ec
Iuty <dfdfggg@126.com>
2020-11-16 23:06:26 +0800

    sigmoid in loss

--- object/mdl_div.py
+++ object/mdl_div.py
@@ -97,7 +97,8 @@ def getLogit(config,image_batch,batch_size):
 def getLoss(config,logits,labels):
     
     with tf.variable_scope('loss') as scope:
-        cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=logits, labels=labels,
+        #sparse_softmax_cross_entropy_with_logits
+        cross_entropy = tf.nn.sigmoid_cross_entropy_with_logits(logits=logits, labels=labels,
                             name='xentropy_per_example')
         loss = tf.reduce_mean(cross_entropy, name='loss')
         tf.summary.scalar(scope.name + '/loss', loss)
